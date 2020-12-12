ea0d3fe9cf4f94e815d115adbaa7165d1e36fba8
Moun Dahiwale <mdahiwale@eshopworld.com>
2020-12-12 11:27:43 +0000

    Initial Service Fabric Projects

---
+++ ECommerce.ProductCatalog.Model/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerce.ProductCatalog.Model
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProducts();
+
+        Task AddProduct(Product product);
+    }
+}
---
+++ ECommerce.ProductCatalog/ServiceFabricProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using ECommerce.ProductCatalog.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.ProductCatalog
+{
+    class ServiceFabricProductRepository : IProductRepository
+    {
+        public Task AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
